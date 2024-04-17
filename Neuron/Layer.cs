39ec6089d2dc62a6c0f8f5ed81197e2e310df519
KochEg <126089352+KochEg@users.noreply.github.com>
2024-04-17 21:32:52 +0700

    edit Layer

--- Neuron/Layer.cs
+++ Neuron/Layer.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Neuron
 {
     public class Layer
     {
+        public List<NeuronOne> Neurons {  get; }   
+        public int Count => Neurons?.Count ?? 0;
+
+        public Layer(List<NeuronOne> neurons, NeuronType type = NeuronType.Normal) 
+        { 
+            //TODO: Проверить все входные нейроны на соответствие типу
+            Neurons = neurons;
+        }
     }
 }
