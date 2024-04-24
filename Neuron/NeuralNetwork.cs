c369a1b5030c71fdcf2c69b1fd2b506121df9cf4
KochEg <126089352+KochEg@users.noreply.github.com>
2024-04-24 20:34:59 +0700

    add Topology

---
+++ Neuron/NeuralNetwork.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Neuron
+{
+    public class NeuralNetwork
+    {
+        public List<Layer> Layers { get; }
+        public Topology Topology { get; }
+
+        public NeuralNetwork(Topology topology)
+        {
+            Topology = topology;
+            Layers = new List<Layer>();
+
+            CreateInputLayer();
+            CreateHiddenLayers();
+            CreateOutputLayer();
+        }
+    }
+}
