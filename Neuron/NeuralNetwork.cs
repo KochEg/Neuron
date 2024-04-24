f0da186007456babd23031c4c1ee36701ad17670
KochEg <126089352+KochEg@users.noreply.github.com>
2024-04-24 20:49:32 +0700

    update

--- Neuron/NeuralNetwork.cs
+++ Neuron/NeuralNetwork.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Neuron
 {
@@ -15,6 +17,36 @@ namespace Neuron
             CreateInputLayer();
             CreateHiddenLayers();
             CreateOutputLayer();
+        }
+
+        private void CreateOutputLayer()
+        {
+            var outputNeurons = new List<NeuronOne>();
+            var lastLayer = Layers.Last();
+            for (int i = 0; i < Topology.OutputCount; i++)
+            {
+                var neuron = new NeuronOne(lastLayer.Count, NeuronType.Output);
+                outputNeurons.Add(neuron);
+            }
+            var outputLayer = new Layer(outputNeurons, NeuronType.Output);
+            Layers.Add(outputLayer);
+        }
+
+        private void CreateHiddenLayers()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateInputLayer()
+        {
+            var inputNeurons = new List<NeuronOne>();
+            for(int i = 0; i < Topology.InputCount; i++)
+            {
+                var neuron = new NeuronOne(1, NeuronType.Input);
+                inputNeurons.Add(neuron);
+            }
+            var inputLayer = new Layer(inputNeurons, NeuronType.Input);
+            Layers.Add(inputLayer);
         }
     }
 }
